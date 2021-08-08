f238fbd8f2bee64f2a5347f030c90ad2d1d1467d
[Python Energy] <carbonleakage@outlook.com>
2021-08-08 23:37:16 +0200

    Cron Commit

--- ebay_berlin.py
+++ ebay_berlin.py
@@ -29,19 +29,19 @@ def db_insert_ebay(db_name,tbl_name, data_list):
     db_conn.close()
     logging.info(f"{dt.datetime.now()} INFO: Inserted {len(data_list)} rows into {tbl_name} table!")
 
+if __name__ == "__main__":
+    db = "/Users/saravananshanmugam/Carbonleakage_Git/UsedObjects/UsedObjects.db"
+    tbl_name = "EBAY_TOP_BERLIN"
+    db_init_tbl_ebay(db, tbl_name)
 
-db = "/Users/saravananshanmugam/Carbonleakage_Git/UsedObjects/UsedObjects.db"
-tbl_name = "EBAY_TOP_BERLIN"
-db_init_tbl_ebay(db, tbl_name)
+    session = HTMLSession()
+    url = "https://www.ebay-kleinanzeigen.de/s-katalog-orte.html?locationId=3331"
 
-session = HTMLSession()
-url = "https://www.ebay-kleinanzeigen.de/s-katalog-orte.html?locationId=3331"
+    response = session.get(url)
+    top_list = response.html.find("li.l-container-row")
 
-response = session.get(url)
-top_list = response.html.find("li.l-container-row")
-
-for sublist in top_list:
-    subrows = [i for i in sublist.text.replace(")","").split("\n")]
-    subrow_pairs = [(i.split("\xa0(")[0], int(i.split("\xa0(")[1].replace(".",""))) for i in subrows]
-    # subrows_dict = {i,j for i,j in }
-    db_insert_ebay(db,tbl_name,subrow_pairs)
+    for sublist in top_list:
+        subrows = [i for i in sublist.text.replace(")","").split("\n")]
+        subrow_pairs = [(i.split("\xa0(")[0], int(i.split("\xa0(")[1].replace(".",""))) for i in subrows]
+        # subrows_dict = {i,j for i,j in }
+        db_insert_ebay(db,tbl_name,subrow_pairs)
