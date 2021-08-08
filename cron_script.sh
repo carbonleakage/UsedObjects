85d378f3a15d99d4c793b9e09088518fb6e9d93c
[Python Energy] <carbonleakage@outlook.com>
2021-08-08 23:50:01 +0200

    Cron Commit

--- cron_script.sh
+++ cron_script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
 ~/opt/miniconda3/bin/python /Users/saravananshanmugam/Carbonleakage_Git/UsedObjects/ebay_berlin.py
-python -m papermill eBay_Overview.ipynb eBay_Overview.ipynb
+~/opt/miniconda3/bin/python papermill eBay_Overview.ipynb eBay_Overview.ipynb
 git add .
 git commit -m "Cron Commit"
 git push origin main
