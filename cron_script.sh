62de897635cda57975d8ed70f4f140321e3e0475
Ubuntu <ubuntu@ubuntu.fritz.box>
2021-08-13 16:30:11 +0200

    Cron Commit

--- cron_script.sh
+++ cron_script.sh
@@ -3,6 +3,6 @@ cd ~/Python_Projects/UsedObjects/
 #source usedobjenv/bin/activate
 ./ebay_berlin.py
 /home/ubuntu/Python_Projects/UsedObjects/usedobjenv/bin/papermill eBay_Overview.ipynb eBay_Overview.ipynb
-#git add .
-#git commit -m "Cron Commit"
-#git push origin main
+git add .
+git commit -m "Cron Commit"
+git push origin main
