1cebedd72e556b46473cc1009f67ebb7ab349ac6
Ubuntu <ubuntu@ubuntu.fritz.box>
2021-08-13 10:07:19 +0200

    Updated papermill command in batch file

--- cron_script.sh
+++ cron_script.sh
@@ -2,7 +2,7 @@
 cd ~/Python_Projects/UsedObjects/
 #source usedobjenv/bin/activate
 ./ebay_berlin.py
-papermill eBay_Overview.ipynb eBay_Overview.ipynb
+/home/ubuntu/Python_Projects/UsedObjects/usedobjenv/bin/papermill eBay_Overview.ipynb eBay_Overview.ipynb
 #git add .
 #git commit -m "Cron Commit"
 #git push origin main
